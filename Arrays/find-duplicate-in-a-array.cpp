4dfd35e5973f18e9c1a849bb0d162cdb78a379d8
Sandeep Gupta <gupta41sandeep@gmail.com>
2017-07-04 00:23:01 +0530

    good-question

---
+++ Arrays/find-duplicate-in-a-array.cpp
@@ -0,0 +1,16 @@
+int Solution::repeatedNumber(const vector<int> &A) {
+     // Sandeep Gupta
+     int slow = A[0];
+      int fast = A[A[0]];
+      while (slow != fast) {
+        slow = A[slow];
+        fast = A[A[fast]];
+      }
+    
+      fast = 0;
+      while (slow != fast) {
+        slow = A[slow];
+        fast = A[fast];
+      }
+      return slow;
+}
